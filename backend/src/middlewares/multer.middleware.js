f3709134c3721f2c5cb978dbc67a674690d2e2ca
Zentroz <ay597555@gmail.com>
2024-08-02 12:29:01 +0530

    minor changes

--- backend/src/middlewares/multer.middleware.js
+++ backend/src/middlewares/multer.middleware.js
@@ -2,7 +2,7 @@ import multer from 'multer'
 
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
-    cb(null, 'E:/Game Dev/Full Stack/Document/pc_e-commerce_MERN/backend/public')
+    cb(null, 'E:/Game Dev/Full Stack/Document/pc_e-commerce_MERN/backend/public/temp')
   },
   filename: function (req, file, cb) {
     const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9)
